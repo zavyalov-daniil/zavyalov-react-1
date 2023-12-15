5478b48ea5307003596a9177af76bc95a3672662
Daniil Zavyalov <dezavv1@gmail.com>
2023-12-15 11:57:51 +0600

    [~] fix header

--- src/components/AppHeader/AppHeader.tsx
+++ src/components/AppHeader/AppHeader.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Logo from "../Logo";
 import classes from "./AppHeader.module.css"
+import clsx from "clsx";
 
 const AppHeader: React.FC = () => {
 
@@ -8,11 +9,26 @@ const AppHeader: React.FC = () => {
         <header className={classes.appHeader}>
             <div className={classes.logo}><Logo/></div>
             <nav className={classes.navigation}>
-                <a className={classes.navigationElement}>Home</a>
-                <a className={classes.navigationElement}>Find a doctor</a>
-                <a className={classes.navigationElement}>Apps</a>
-                <a className={classes.navigationElement}>Testimonials</a>
-                <a className={classes.navigationElement}>About us</a>
+                <a className={
+                    clsx(classes.navigationElement,
+                        classes.bold)
+                }>Home</a>
+                <a className={
+                    clsx(classes.navigationElement,
+                        classes.regular)
+                }>Find a doctor</a>
+                <a className={
+                    clsx(classes.navigationElement,
+                        classes.regular)
+                }>Apps</a>
+                <a className={
+                    clsx(classes.navigationElement,
+                        classes.regular)
+                }>Testimonials</a>
+                <a className={
+                    clsx(classes.navigationElement,
+                        classes.regular)
+                }>About us</a>
             </nav>
         </header>
     )
